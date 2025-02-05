e50bd9971340cc8d0abe3af880b3332b7e4dce80
Juanchivp12 <Juanvillegasse@gmail.com>
2025-02-05 14:36:12 -0600

    Some progress
    - Added some constants.
    - Named the game
    - Made a Paddle class

--- game.py
+++ game.py
@@ -2,12 +2,31 @@ import pygame
 
 pygame.init()
 
-WIDTH, HEIGHT = 1280, 720
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
 running = True
 
+WIDTH, HEIGHT = 1280, 720
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+PADDLE_SPEED = 10
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+
+# Setup screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+# Paddle class
+class Paddle:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT)
+    
+    # Method for moving
+
+    # Method for drawing the paddle
+
+# Main game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # when user clicks x, closes game
@@ -21,7 +40,7 @@ while running:
     pygame.display.flip()
 
     # limit fps to 60
-    clock.tick(60)
+    clock.tick(FPS)
 
 pygame.quit()
 
